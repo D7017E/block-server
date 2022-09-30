0a61cce78a03c8063b8cf5aa95afb1489d829bac
Hugo Pettersson <51755959+HugPet@users.noreply.github.com>
2022-09-30 15:04:08 +0200

    Added pepper movement, translation, eye expressions

---
+++ behaviors/movement/translation/translation.py
@@ -0,0 +1,57 @@
+import random
+import time
+import threading
+
+class PepperMove():
+
+    def __init__(self, service):
+        self.stop_id = None
+        self.service = service
+        self.thread = None
+        self.lock = threading.Lock()
+
+    def move(self, x, y, angle, timeout):
+        """
+        Move which takes in a
+        * x speed, positive means forward, negative means backward
+        * y speed, positive means left, negative means right
+        * angle speed, positive means rotate left, negative means rotate right
+        * timeout
+        x and y max speed is 0.35 which is 35 cm per second.
+        Meaning that if setting the timeout to 3 it moves 105 cm forward, (0.35 * 3)
+        
+        angle max speed is about 58 degrees per second, but is set as percentage.
+        1 is 58 degrees per second. passing in 6.25s, then the robot will do one revolution
+        """
+
+        # set moving speed
+        self.service.move(x, y, angle)
+        stop_id = random.randint(0, 1000000)
+        # acquire lock for stop id and thread
+        self.lock.acquire()
+        self.stop_id = stop_id
+        if self.thread is not None:
+            self.thread.join()
+        self.thread = threading.Thread(
+            target=self.__stop_movement,
+            args=(stop_id, timeout))
+        self.thread.start()
+        self.lock.release()
+
+    def stop_movement(self):
+        """
+        Will stop movement right away and stop all treads running
+        """
+        self.service.move(0, 0, 0)
+        self.lock.acquire()
+        if self.thread is not None:
+            self.thread.join()
+        self.stop_id = None
+        self.lock.release()
+
+    def __stop_movement(self, stop_id, timeout):
+        time.sleep(timeout)
+        self.lock.acquire()
+        if stop_id == self.stop_id:
+            self.service.move(0, 0, 0)
+        self.lock.release()
---
+++ main.py
@@ -0,0 +1,48 @@
+from shutil import move
+from PepperConnection import PepperConnection
+from behaviors.expressions.expression import PepperExpression
+from behaviors.movement.translation.translation import PepperMove
+from behaviors.speech.PepperSpeech import PepperSpeech
+import time
+import os
+
+def main():
+
+    conn = PepperConnection("130.240.238.32", 9559, "nao", os.getenv("password"))
+
+    conn.connect()
+
+    tts_service = conn.get_speech_service()
+    move_service = conn.get_move_service()
+    auto_service = conn.get_autonomous_service()
+    battery_service = conn.get_battery_service()
+    led_service = conn.get_led_service()
+    
+    # default state is "interactive"
+    auto_service.setState("safeguard")
+    # auto_service.setState("interactive")
+    print(auto_service.getState())
+    print(auto_service.focusedActivity())
+    print(auto_service.getAutonomousActivityStatistics())
+    print("battery level is: {}%".format(battery_service.getBatteryCharge()))
+    # auto_service.stopFocus()
+    print(auto_service.focusedActivity())
+
+    # move_service.setOrthogonalSecurityDistance(0.4)
+    # move_service.move(0.25,0,0)
+    # time.sleep(3)
+    # move_service.move(0,0,0)
+    # return
+
+    # pepperMove = PepperMove(move_service)
+    # pepperMove.move(0.2, -0.1, 1, 10)
+
+    pepExpr = PepperExpression(0xffffff, led_service)
+    # pepExpr.fade_eyes(led_service, 0xff0000, 1)
+    pepExpr.random_eyes(5)
+    # pepExpr.blink_eyes(0.10)
+    while True:
+        time.sleep(1)
+    # move_service.move(0,0,0)
+
+main()
