97346007abd9e8967be7a7eb0a14268d95bda582
Johan Olsson <joholsprivat@gmail.com>
2022-10-31 14:06:03 +0100

    Added lint fixes for handle code and behaviurs

--- handle_code/blockly_connection/server.py
+++ handle_code/blockly_connection/server.py
@@ -1,23 +1,25 @@
+"""
+This module handles the api calls with flask
+"""
 from flask import Flask, request, jsonify
 from handle_code.queue.queue import Queue
 
-app = Flask("API")
-server = None
+APP = Flask("API")
 
-@app.route('/code', methods=['POST'])
+@APP.route('/code', methods=['POST'])
 def __post_code():
     """
     This method is called from the Flask application, it handles the incoming post request
     """
-    if request.data is "":
+    if request.data == "":
         return jsonify(success=False, message="No body was received")
     program = request.data
     queue_length = Queue.add_program_to_queue(program)
-    message = ("Successfully added your program to the queue," +
-        " there are currently {} programs before you").format(queue_length - 1)
+    message = "Successfully added your program to the queue"
     return jsonify(
         success=True,
-        message=message)
+        message=message,
+        queueLength=queue_length)
 
 def start_server(port=5000):
     """
@@ -25,9 +27,8 @@ def start_server(port=5000):
 
     Starts a server which listens on port
     """
-    app.run(port=port, host="0.0.0.0", load_dotenv=False, use_reloader=False)
+    APP.run(port=port, host="0.0.0.0", load_dotenv=False, use_reloader=False)
 
 if __name__ == "__main__":
     # Development
-    app.run(port=5000, host="0.0.0.0", load_dotenv=False, use_reloader=False)
-
+    APP.run(port=5000, host="0.0.0.0", load_dotenv=False, use_reloader=False)
