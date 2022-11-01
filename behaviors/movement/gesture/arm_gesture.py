6f0b4e3b1094738cf564950bc9c521afba6da3ce
Hugo Pettersson <51755959+HugPet@users.noreply.github.com>
2022-11-01 12:19:18 +0100

    fix pylint for some files

--- behaviors/movement/gesture/arm_gesture.py
+++ behaviors/movement/gesture/arm_gesture.py
@@ -1,11 +1,26 @@
+"""
+File for gesticulating the arms of Pepper.
+"""
 
+class ArmGesture():
+    """
+    Class for gesticulating the arms of pepper.
+    """
 
-class Arm_gesture():
-    
     def __init__(self, service):
+        """
+        * <service>, a motion_service from naoqi.
+        Initializes the ArmGesture object.
+        """
         self.service = service
-    
+
     def move_left_arm(self, roll, speed, degrees):
+        """
+        * <roll> boolean, indicates whether the axis of movement is roll (true) or pitch (false).
+        * <speed> integer, the speed at which the movement occurs. Between [0, 100].
+        * <degrees> integer, the angle which the arm should move to.
+        Moves the left arm to a certain angle with a certain speed.
+        """
         if speed <= 0 or speed > 100:
             print("Wrong input for speed, only accepts (0, 100]")
             return
@@ -24,6 +39,12 @@ class Arm_gesture():
             self.service.setAngles("LShoulderPitch", angle, speed)
 
     def move_right_arm(self, roll, speed, degrees):
+        """
+        * <roll> boolean, indicates whether the axis of movement is roll (true) or pitch (false).
+        * <speed> integer, the speed at which the movement occurs. Between [0, 100].
+        * <degrees> integer, the angle which the arm should move to.
+        Moves the right arm to a certain angle with a certain speed.
+        """
         if speed <= 0 or speed > 100:
             print("Wrong input for speed, only accepts (0, 100]")
             return
--- main.py
+++ main.py
@@ -1,23 +1,38 @@
-from PepperConnection import PepperConnection
-from behaviors.expressions.expression import PepperExpression
-from behaviors.movement.gesture.head_gesture import Head_gesture
-from behaviors.movement.gesture.arm_gesture import Arm_gesture
-from behaviors.movement.translation.translation import PepperMove
-from behaviors.speech.PepperSpeech import PepperSpeech
-from handle_code.blockly_connection import server
-from handle_code.queue.queue import Queue
+"""
+Main file, used to initialize connections.
+"""
+
+
 import time
 import os
 import threading
 import sys
+from numpy import empty
+from PepperConnection import PepperConnection
+from behaviors.expressions.expression import PepperExpression
+from behaviors.movement.gesture.head_gesture import HeadGesture
+from behaviors.movement.gesture.arm_gesture import ArmGesture
+from behaviors.movement.translation.translation import PepperMove
+# from behaviors.speech.PepperSpeech import PepperSpeech
+from handle_code.blockly_connection import server
+from handle_code.queue.queue import Queue
 
 class Main:
+    """
+    Main class
+    """
     def __init__(self):
+        """
+        Sets start variables and calls main method.
+        """
         self.should_run = True
         self.connect_to_pepper = True
         self.main()
 
     def main(self):
+        """
+        Starts a thread for popping programs from the queue, and starts listening on server.
+        """
         threading.Thread(target=self.run).start()
         server.start_server(5020)
         print("Interrupted by user, shutting down")
@@ -26,21 +41,23 @@ class Main:
         sys.exit(0)
 
     def run(self):
+        """
+        Connects to pepper, and pops programs from the queue.
+        """
         if self.connect_to_pepper:
             conn = PepperConnection("130.240.238.32", 9559, "nao", os.getenv("password"))
 
             conn.connect()
 
-            tts_service = conn.get_speech_service()
+            # tts_service = conn.get_speech_service()
             motion_service = conn.get_motion_service()
             auto_service = conn.get_autonomous_service()
             battery_service = conn.get_battery_service()
-            led_service = conn.get_led_service()
-            audio_service = conn.get_audio_service()
-            tablet_service = conn.get_tablet_service()
+            # led_service = conn.get_led_service()
+            # audio_service = conn.get_audio_service()
+            # tablet_service = conn.get_tablet_service()
             behavior_service = conn.get_behavior_service()
             blinking_service = conn.get_blinking_service()
-            
             # default state is "interactive"
             auto_service.setState("safeguard")
             motion_service.setIdlePostureEnabled("Head", False)
@@ -56,35 +73,33 @@ class Main:
             print("battery level is: {}%".format(battery_service.getBatteryCharge()))
             # auto_service.stopFocus()
             print(auto_service.focusedActivity())
-            
             # arm_ges.move_left_arm(False, 100, 90)
             # arm_ges.move_right_arm(False, 100, 90)
             # ges.reset_head()
             # threading.Thread(target=ges.spin_head, args=(10,)).start()
-            pepExpr = PepperExpression(0xffffff, led_service)
+            pep_expr = PepperExpression(0xffffff, conn.get_led_service())
             # pepExpr.random_eyes(10)
 
-            
+
             # move_service.setOrthogonalSecurityDistance(0.4)
             # move_service.move(0.25,0,0)
             # time.sleep(3)
             # move_service.move(0,0,0)
             # return
-            pepExpr.angry_eyes()
-            head_ges = Head_gesture(motion_service)
+            pep_expr.angry_eyes()
+            head_ges = HeadGesture(motion_service)
             head_ges.reset_head()
 
 
-            pepperMove = PepperMove(motion_service)
-            pepperMove.move(0,0,0.5,3)
+            pepper_move = PepperMove(motion_service)
+            pepper_move.move(0, 0, 0.5, 3)
             time.sleep(2.5)
-            pepperMove.move(0,0,1,10)
+            pepper_move.move(0, 0, 1, 10)
 
-            arm_ges = Arm_gesture(motion_service)
+            arm_ges = ArmGesture(motion_service)
             arm_ges.move_left_arm(True, 100, 89.5)
             time.sleep(0.1)
-            arm_ges.move_right_arm(True, 100, -89.5)   
-
+            arm_ges.move_right_arm(True, 100, -89.5)
             # pepperMove.move(0.2, -0.1, 1, 10)
 
 
@@ -97,26 +112,29 @@ class Main:
             # pepExpr.squint_eyes(1)
             # pepExpr.blink_eyes(0.10)
         while self.should_run:
-                time.sleep(1)
-                program = Queue.get_next_program()
-                if program is not None:
-                    self.executeProgram(program)
+            time.sleep(1)
+            program = Queue.get_next_program()
+            if program is not None:
+                execute_program(program)
         if self.connect_to_pepper:
             print("Resetting pepper")
             head_ges.reset_head()
             arm_ges.move_left_arm(True, 100, 0.5)
-            arm_ges.move_right_arm(True, 100, -0.5)   
+            arm_ges.move_right_arm(True, 100, -0.5)
             arm_ges.move_left_arm(False, 100, 90)
             arm_ges.move_right_arm(False, 100, 90)
-            pepExpr.fade_eyes(0xffffff, 1)
-            pepperMove.stop_movement()
+            pep_expr.fade_eyes(0xffffff, 1)
+            pepper_move.stop_movement()
             time.sleep(0.4)
-    
-    def executeProgram(self, program):
-        if len(program) == 0:
-            return
-        # TODO: Do something before? Add some timer? Run in thread? Check code?
-        exec(program)
-        time.sleep(5)
+
+def execute_program(program):
+    """
+    Executes a program with the help of exec.
+    """
+    if program is empty:
+        return
+    # TODO: Do something before? Add some timer? Run in thread? Check code?
+    exec(program) # pylint: disable=exec-used
+    time.sleep(5)
 
 Main()
