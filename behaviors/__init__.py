24f3d35e34094c4053c91788e286cbaede317c77
Johan Olsson <51018314+JohanOlsson99@users.noreply.github.com>
2022-11-22 15:57:23 +0100

    Rename of file to follow the class name, snake case fix (#43)

--- behaviors/__init__.py
+++ behaviors/__init__.py
@@ -4,4 +4,4 @@ sys.dont_write_bytecode = True
 from expressions import PepperExpression
 from movement import ArmGesture, HeadGesture, HipGesture, PepperMove
 from speech import PepperSpeech
-from composite_functions import CompositeHandler
+from composite_handler import CompositeHandler
